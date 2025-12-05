a4de1519ff3ac88193731539a1e2ad58ab270e80
MIRELLA SAMPAIO <mirellasampaio63@gmail.com>
2025-12-05 20:12:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,4 @@ st.write(
     "O que fazer para o Brasil ter uma Educação de Qualidade? "
 )
 st.subheader("Para uma educação de qualidade no Brasil," \
-" é preciso valorizar e formar professores, investir em infraestrutura escolar (bibliotecas, laboratórios), focar no aluno como centro (escutando suas necessidades) " \
-"e garantir acesso universal e equidade, com políticas públicas integradas (saúde, assistência social) e uso de tecnologia," \
-" focando em metodologias ativas e ambiente estimulante, além de promover a participação da comunidade e avaliação contínua, " \
-"indo além do mero aumento de verba. ")
+" é preciso valorizar e formar professores,para saber mais veja no site a seguir... ")
